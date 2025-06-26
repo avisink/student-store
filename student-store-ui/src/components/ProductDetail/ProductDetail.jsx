ff5400a465513fb85028582d4e3e396ee9720c16
avisink <ayomidevanessa@gmail.com>
2025-06-25 18:25:56 -0700

    Added use effect hook for functionality and display

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -8,7 +8,7 @@ import "./ProductDetail.css";
 function ProductDetail({ addToCart, removeFromCart, getQuantityOfItemInCart }) {
   
   const { productId } = useParams();
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState({});
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState(null);
 
@@ -35,11 +35,33 @@ function ProductDetail({ addToCart, removeFromCart, getQuantityOfItemInCart }) {
     }
   };
 
+  useEffect(() => {
+    console.log("fetching or not? tf");
+    const fetchProductDetails = async () => {
+      // setProduct(null)
+      // setIsFetching(true); these 2 lines totally messed up my product rendering, ensure to delete
+      setError(null);
+      try {
+        const res = await axios.get(`http://localhost:5000/products/${productId}`);
+        setProduct(res.data);
+        console.log(res.data);
+      } catch (err) {
+        setError("Could not fetch product details, sorry :( ");
+      } finally {
+        setIsFetching(false);
+      }
+    };
+    fetchProductDetails();
+  }, [productId]);
+
   return (
     <div className="ProductDetail">
       <div className="product-card">
         <div className="media">
-          <img src={product.image_url || "/placeholder.png"} alt={product.name} />
+          <img
+            src={product.image_url || "/assets/codepath.svg"}
+            alt={product.name}
+          />
         </div>
         <div className="product-info">
           <p className="product-name">{product.name}</p>
@@ -47,8 +69,12 @@ function ProductDetail({ addToCart, removeFromCart, getQuantityOfItemInCart }) {
           <p className="description">{product.description}</p>
           <div className="actions">
             <button onClick={handleAddToCart}>Add to Cart</button>
-            {quantity > 0 && <button onClick={handleRemoveFromCart}>Remove from Cart</button>}
-            {quantity > 0 && <span className="quantity">Quantity: {quantity}</span>}
+            {quantity > 0 && (
+              <button onClick={handleRemoveFromCart}>Remove from Cart</button>
+            )}
+            {quantity > 0 && (
+              <span className="quantity">Quantity: {quantity}</span>
+            )}
           </div>
         </div>
       </div>
