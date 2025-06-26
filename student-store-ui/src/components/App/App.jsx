32f5f8f69eadc1a0b1963658f4119707c40c611c
avisink <ayomidevanessa@gmail.com>
2025-06-25 18:22:08 -0700

    Added required functionality to handle checkout, and to populate the product grid with products in the db

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,30 +3,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import SubNavbar from "../SubNavbar/SubNavbar";
 import Sidebar from "../Sidebar/Sidebar";
+import PastOrders from "../PastOrders/PastOrders";
+import OrderDetails from "../OrderDetails/OrderDetails";
 import Home from "../Home/Home";
 import ProductDetail from "../ProductDetail/ProductDetail";
 import NotFound from "../NotFound/NotFound";
-import { removeFromCart, addToCart, getQuantityOfItemInCart, getTotalItemsInCart } from "../../utils/cart";
+import { calculateTotal } from "../../utils/calculations";
+import { removeFromCart, addToCart, getQuantityOfItemInCart, getTotalItemsInCart } from "../../utils/cart"; //i need to use these somehow in handling checkout
 import "./App.css";
 
 function App() {
 
-  // State variables
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [activeCategory, setActiveCategory] = useState("All Categories");
   const [searchInputValue, setSearchInputValue] = useState("");
-  const [userInfo, setUserInfo] = useState({ name: "", dorm_number: ""});
+  const [userInfo, setUserInfo] = useState({ customer_id: "", email: ""});
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
   const [isFetching, setIsFetching] = useState(false);
   const [isCheckingOut, setIsCheckingOut] = useState(false);
   const [error, setError] = useState(null);
+  const [pastOrder, setPastOrder] = useState(null);
   const [order, setOrder] = useState(null);
 
-  // Toggles sidebar
   const toggleSidebar = () => setSidebarOpen((isOpen) => !isOpen);
 
-  // Functions to change state (used for lifting state)
   const handleOnRemoveFromCart = (item) => setCart(removeFromCart(cart, item));
   const handleOnAddToCart = (item) => setCart(addToCart(cart, item));
   const handleGetItemQuantity = (item) => getQuantityOfItemInCart(cart, item);
@@ -36,9 +37,84 @@ function App() {
     setSearchInputValue(event.target.value);
   };
 
+  /// complete handle checkout, this is where i set setischecking out bc 
+  // i think thats the only state declared and not used in this file yet. 
+  // refer to the slides
   const handleOnCheckout = async () => {
+    setIsCheckingOut(true);
+    setError(null);
+
+    //customer_id: input validashunnn
+    const customerId = Number(userInfo.customer_id);
+    if (
+      !userInfo.customer_id ||
+      isNaN(customerId) ||
+      !Number.isInteger(customerId)
+    ) {
+      setError("Please enter a valid numeric Student ID.");
+      setIsCheckingOut(false);
+      return;
+    }
+
+    const cartItems = Object.entries(cart).map(([productId, quantity]) => {
+      const product = products.find((p) => p.id === Number(productId));
+      return {
+        product_id: Number(productId),
+        quantity,
+        price: product?.price || 0,
+      };
+    });
+
+    //i think the backend takes care of the calculation already, but to be safe
+    const subTotal = cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+    const totalWithTaxes = calculateTotal(subTotal); 
+    const total_price = Number(totalWithTaxes.toFixed(2)); 
+
+    //creating the order
+    try {
+      console.log({ customerId, total_price });
+      const orderRes = await axios.post("http://localhost:5000/orders", {
+        customer_id: customerId,
+        total_price,
+        status: "created",
+      });
+      const order = orderRes.data;
+      console.log(orderRes.data); //debugging
+      const order_id = order.order_id;
+
+      //creating order itemss
+      await axios.post(
+        `http://localhost:5000/orders/${order_id}/items`,
+        cartItems
+      );
+
+      setOrder(order);
+      setCart({});
+    } catch (err) {
+      setError("Could not place order");
+    } finally {
+      setIsCheckingOut(false);
+    }
   }
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsFetching(true);
+      setError(null);
+      try {
+        const res = await axios.get(`http://localhost:5000/products`);
+        setProducts(res.data);
+      } catch (err) {
+        setError("Could not fetch products");
+      } finally {
+        setIsFetching(false);
+      }
+    };
+    fetchProducts();
+  }, []); 
 
   return (
     <div className="App">
@@ -97,6 +173,16 @@ function App() {
                 />
               }
             />
+            <Route 
+            path="/past-orders" 
+            element={<PastOrders />} 
+            />
+
+            <Route 
+            path="/past-orders/:orderId" 
+            element={<OrderDetails 
+            />} />
+
             <Route
               path="*"
               element={
